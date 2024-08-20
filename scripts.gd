83923b103562d0e8db6c4e3d653d58f69343b7ae
jacob18011871 <frueanj@wsc.school.nz>
2024-08-20 14:32:47 +1200

    Update scripts.gd

--- scripts.gd
+++ scripts.gd
@@ -6,7 +6,7 @@ const JUMP_VELOCITY = -200.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@onready var sprite = "res://player.tscn::AtlasTexture_xtn7b"
+
 
 func _physics_process(delta):
 	# Add the gravity.
