fe64997da2a4566fd3054c07bf053752755575bb
jacob18011871 <frueanj@wsc.school.nz>
2024-08-27 15:10:36 +1200

    Added animations, death and a bit of worldbuilding

--- scripts.gd
+++ scripts.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
 
-const SPEED = 100.0
+const SPEED = 70.0
 const JUMP_VELOCITY = -200.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var sprite = $"AnimatedSprite2D"
+
 
 
 func _physics_process(delta):
@@ -16,14 +18,34 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
+	# jump aniomation 
+	if gravity == 1:
+		sprite.play("jump")
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
+	
+	# Flip character
+	if direction == 1.00:
+		sprite.flip_h = false
+	elif direction == -1.00:
+		sprite.flip_h = true
+	# Play movement
+	if direction == 0.00:
+		sprite.play("idle")
+	else:
+		sprite.play("walk")
+	
+	# Apply movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	# Sprint
+	
 
 	move_and_slide()
 	
